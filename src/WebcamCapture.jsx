692b6216714186adee039c2b9ebea3dc9144c5e6
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2019-11-15 16:21:23 +0100

    Make webcam small.

--- src/WebcamCapture.jsx
+++ src/WebcamCapture.jsx
@@ -18,10 +18,9 @@ const WebcamCapture = () => {
 
   return (<React.Fragment >
     <Webcam audio={false}
-      height={720}
       ref={webcamRef}
       screenshotFormat="image/jpeg"
-      width={1280}
+      width={200}
       videoConstraints={videoConstraints}
     /> <button onClick={capture} > Capture photo </button>
   </React.Fragment >
