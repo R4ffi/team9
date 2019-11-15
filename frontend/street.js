3f06a9b6d570280c26b4cb4670cca90961e5feea
Gabriel Frischknecht <gabrieljeremias.frischknecht@students.bfh.ch>
2019-11-15 14:50:34 +0100

    added cars and json with list of cars

--- frontend/street.js
+++ frontend/street.js
@@ -10,7 +10,7 @@ class Street{
   display() {
     this.firstLane = (this.canvasWidth - (this.lanes * this.laneWidth)) /2
     let i = 0;
-    for(i; i <= this.lanes; i ++){
+    for(i; i <= this.lanes; i++){
       line(this.firstLane,this.canvasHeight,this.firstLane, 0) 
       this.firstLane += this.laneWidth;
     }
