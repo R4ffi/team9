cbe1da3f2fa12b1a337181d5a4b580a35e5d4884
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2019-11-16 07:45:33 +0100

    D History schön gmacht

--- src/History.jsx
+++ src/History.jsx
@@ -5,29 +5,38 @@ export const History = ({ history, emotions }) => {
   let items = [];
 
   for (let item of history) {
-    console.log(item);
-
     var nextEmotion = _.find(emotions, (e => parseInt(e.timestamp) > parseInt(item.timestamp)));
     if (nextEmotion) {
       items = [...items, { timestamp: item.timestamp, imagePath: item.imagePath, emotion: nextEmotion.emotion }];
-      console.log("imagePath: " + item.imagePath);
-    } else {
-      console.log('no emotion found: ' + item.timestamp)
     }
   }
 
   return (
-    <React.Fragment>
-      <table>
-        <tbody>
-          <tr>
-            {items.map((item, index) => <td key={index}><img style={{ width: '60px' }} src={item.imagePath} alt={item.imagePath} /></td>)}
-          </tr>
-          <tr>
-            {items.map((item, index) => <td key={index}><img style={{ width: '60px' }} src={`assets/emotions/${item.emotion}.png`} alt={item.emotion} /></td>)}
-          </tr>
-        </tbody>
-      </table>
-    </React.Fragment >
+    <div style={{ textAlign: "center", display: "inline-block" }}>
+      <h3>Wir haben diene Stimmung während er Fahrt aufgezeichnet.</h3>
+      <h4>Erfahre welche Entscheidungen dich glücklich gemacht haben.</h4>
+      <div style={{ textAlign: "center", display: "inline-block" }}>
+        <table>
+          <tbody>
+            {
+              items.map((item, index) =>
+                <tr key={index}>
+                  <td >
+                    <img style={{ height: '80px' }} src={item.imagePath} alt={item.imagePath} />
+                  </td>
+                  <td style={{ width: '100px' }} >
+                    <h1>=</h1>
+                  </td>
+                  <td >
+                    <img style={{ height: '80px' }} src={`assets/emotions/${item.emotion}.png`} alt={item.emotion} />
+                  </td>
+                </tr>
+              )
+            }
+
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
