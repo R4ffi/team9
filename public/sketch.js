484d251e40da20e8b5e5d2f276446dca90c40939
Gabriel Frischknecht <gabrieljeremias.frischknecht@students.bfh.ch>
2019-11-16 03:42:26 +0100

    added color

--- public/sketch.js
+++ public/sketch.js
@@ -94,6 +94,14 @@ function setup() {
     });
     hammer.on("swipe", swiped);
     button = createButton("Start");
+    button.style('background-color', '#E8800F');
+    button.style('border', 'none');
+    button.style('color', 'white');
+    button.style('padding', '15px 32px');
+    button.style('text-align', 'center');
+    button.style('text-decoration', 'none');
+    button.style('display', 'inline-block');
+    button.style('font-size', '16px');
     button.position(displayWidth/2, displayHeight/2);
     button.mousePressed(start); 
    
